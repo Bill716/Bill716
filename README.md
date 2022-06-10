bb4146eaa3ecd4993755bc921bd4ab5595c24bde
Mr.Bill_Hub <75666113+Bill716@users.noreply.github.com>
2022-06-10 15:36:00 +0200

    Read Me update

--- README.md
+++ README.md
@@ -1,55 +1,53 @@
 ## Hi there 👋 <img align="right" style="border: 2px solid red; border-radius: 12px;" src="https://media.discordapp.net/attachments/836279726003322991/870031250516217866/abdala.png?width=712&height=702" width="180" />
-I'm **Bill716**, An developer from somewhere in the earth. I like to code web applications and games. I have worked on many projects in my past. Thanks for visiting my github profile. Have a great day ahead!~
-
-<h2 align="center">✨ About Me ✨</h2>
-
-```js
-const Bill716 = {
-    FavouriteLanguage: "Javascript/Typescript",
-    OpenedIssues: 76,
-    OpenedPullRequests: 54,
-    TotalCommits: 5496,
-    Stars: 1536,
-    Repositories: {
-       Created: 76,
-       Contributed: 50
-    },
-}; //I'm a Epic Object, UwU
-```
-
-<h2 align="center">💬 Contact Me 💬</h2>
-
-[![Discord Presence](https://lanyard.cnrad.dev/api/640512148786642947)](https://discord.com/users/640512148786642947)
-
-<details>
-    <summary>
-        Support me
-    </summary>
-    <br />
-    <a href="https://ko-fi.com/bill_hub" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-red.png" alt="Buy Me A Coffee" width="150" ></a>
-</details>
-
-<h2 align="center">🚀 My Stats 🚀</h2>
-<p align="center">
-    <img src="https://github-readme-streak-stats.herokuapp.com/?user=SudhanPlayz&theme=tokyonight" />
-</p>
-<details>
-    <summary>
-        Even more stats
-    </summary>
-    <br />
-    <p align="center">
-        <img src="https://github-profile-trophy.vercel.app/?username=SudhanPlayz&theme=dracula" />
-    </p>
-    <p align="center">
-        <img src="https://github-readme-stats.vercel.app/api?username=Bill716&theme=tokyonight&count_private=true&show_icons=true&include_all_commits=true" />
-    </p>
-    <p align="center">
-        <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=stuyy&layout=compact&theme=dark" />
-    </p>
-</details>
-
-<br />
-
-<!-- Last updated on Sat Jan 22 2022 18:16:37 GMT+0000 (Coordinated Universal Time) ;-;-->
+  I'm **Bill716**, An developer from somewhere in the earth. I like to code web applications and games. I have worked on many projects in my past. Thanks for visiting my github profile. Have a great day ahead!~
+  
+  <h2 align="center">✨ About Me ✨</h2>
+  
+  ```js
+  const Bill716 = {
+      FavouriteLanguage: "Javascript/Typescript",
+      OpenedIssues: 76,
+      OpenedPullRequests: 54,
+      TotalCommits: 5496,
+      Stars: 1536,
+      Repositories: {
+         Created: 76,
+         Contributed: 50
+      },
+  }; //I'm a Epic Object, UwU
+  ```
+  
+  <h2 align="center">💬 Contact Me 💬</h2>
+  
+  [![Discord Presence](https://lanyard.cnrad.dev/api/640512148786642947)](https://discord.com/users/640512148786642947)
+  
+  <details>
+      <summary>
+          Support me
+      </summary>
+      <br />
+      <a href="https://ko-fi.com/bill_hub" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-red.png" alt="Buy Me A Coffee" width="150" ></a>
+  </details>
+  
+  <h2 align="center">🚀 My Stats 🚀</h2>
+  <p align="center">
+      <img src="https://github-readme-streak-stats.herokuapp.com/?user=SudhanPlayz&theme=tokyonight" />
+  </p>
+  <details>
+      <summary>
+          Even more stats
+      </summary>
+      <br />
+      <p align="center">
+          <img src="https://github-profile-trophy.vercel.app/?username=SudhanPlayz&theme=dracula" />
+      </p>
+      <p align="center">
+          <img src="https://github-readme-stats.vercel.app/api?username=Bill716&theme=tokyonight&count_private=true&show_icons=true&include_all_commits=true" />
+      </p>
+      <p align="center">
+          <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=stuyy&layout=compact&theme=dark" />
+      </p>
+  </details>
+  
+<!-- Last updated on Fri Jun 10 2022 15:32:37 GMT+0200 (Central European Summer Time) ;-;-->
 <i>Last updated on 10th June 2022 using magic</i> ✨
--- src/update.js
+++ src/update.js
@@ -9,7 +9,7 @@ let special;
 
 const CountStars = async () => {
   let StarsData = await fetch(
-    `https://api.github.com/users/SudhanPlayz/starred?per_page=100&page=${page}`
+    `https://api.github.com/users/Bill716/starred?per_page=100&page=${page}`
   ).then((res) => res.json());
   stars += StarsData.length;
   page++;
@@ -28,45 +28,61 @@ const WriteReadMe = async () => {
   if(mm === 12)special = ["⛄", "❄", "🎄"]
 
   //Fetching Info From Github API
-  let UserData = await fetch("https://api.github.com/users/SudhanPlayz").then(
+  let UserData = await fetch("https://api.github.com/users/Bill716").then(
     (res) => res.json()
   );
 
   //Creating the text what we gonna save on ReadMe file
-  const text = `## Hi there 👋 <img align="right" src="https://avatars.githubusercontent.com/u/55418697?v=4" width="200" />
-I'm **Sudhan**, An developer from somewhere in the earth. I like to code web applications and games. I have worked on many projects in my past, Some of my open source projects are pinned below make sure to check them out.
+  const text = `## Hi there 👋 <img align="right" style="border: 2px solid red; border-radius: 12px;" src="https://media.discordapp.net/attachments/836279726003322991/870031250516217866/abdala.png?width=712&height=702" width="180" />
+  I'm **Bill716**, An developer from somewhere in the earth. I like to code web applications and games. I have worked on many projects in my past. Thanks for visiting my github profile. Have a great day ahead!~
   
-Thanks for visiting my github profile. Have a great day ahead!~
+  <h2 align="center">✨ About Me ✨</h2>
   
-<h2 align="center"> ${special?special[0]:"✨"} About Me ${special?special[0]:"✨"}</h2>
-
-\`\`\`js
-const Sudhan = {
-    FavouriteLanguage: "Javascript/Typescript",
-    OpenedIssues: {{ ISSUES }},
-    OpenedPullRequests: {{ PULL_REQUESTS }},
-    TotalCommits: {{ COMMITS }},
-    Stars: ${stars},
-    Repositories: {
-       Created: {{ REPOSITORIES }},
-       Contributed: {{ REPOSITORIES_CONTRIBUTED_TO }}
-    },
-}; //I'm a Epic Object, UwU
-\`\`\`
+  \`\`\`js
+  const Bill716 = {
+      FavouriteLanguage: "Javascript/Typescript",
+      OpenedIssues: 76,
+      OpenedPullRequests: 54,
+      TotalCommits: 5496,
+      Stars: 1536,
+      Repositories: {
+         Created: 76,
+         Contributed: 50
+      },
+  }; //I'm a Epic Object, UwU
+  \`\`\`
   
-<h2 align="center"> ${special?special[1]:"🚀"} My Stats ${special?special[1]:"🚀"}</h2>
-<p align="center">
-<img src="https://github-readme-streak-stats.herokuapp.com/?user=SudhanPlayz&theme=tokyonight">
-</p>
-<details>
-  <summary>
-      Even more stats
-  </summary>
+  <h2 align="center">💬 Contact Me 💬</h2>
+  
+  [![Discord Presence](https://lanyard.cnrad.dev/api/640512148786642947)](https://discord.com/users/640512148786642947)
+  
+  <details>
+      <summary>
+          Support me
+      </summary>
+      <br />
+      <a href="https://ko-fi.com/bill_hub" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-red.png" alt="Buy Me A Coffee" width="150" ></a>
+  </details>
+  
+  <h2 align="center">🚀 My Stats 🚀</h2>
   <p align="center">
-    <img src="https://github-profile-trophy.vercel.app/?username=SudhanPlayz&theme=dracula">
-    <img src="https://github-readme-stats.vercel.app/api?username=SudhanPlayz&theme=tokyonight">
+      <img src="https://github-readme-streak-stats.herokuapp.com/?user=SudhanPlayz&theme=tokyonight" />
   </p>
-</details>
+  <details>
+      <summary>
+          Even more stats
+      </summary>
+      <br />
+      <p align="center">
+          <img src="https://github-profile-trophy.vercel.app/?username=SudhanPlayz&theme=dracula" />
+      </p>
+      <p align="center">
+          <img src="https://github-readme-stats.vercel.app/api?username=Bill716&theme=tokyonight&count_private=true&show_icons=true&include_all_commits=true" />
+      </p>
+      <p align="center">
+          <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=stuyy&layout=compact&theme=dark" />
+      </p>
+  </details>
   
 <!-- Last updated on ${date.toString()} ;-;-->
 <i>Last updated on ${date.getDate()}${
